6761e639d725a6cdc5998dd41a4ac8a4ffbf7ab6
Aitmatow <Aitmatow_Demo_PP@github.com>
2018-02-02 10:20:56 +0600

    kkk

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -44,11 +44,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 166);
+            this.label1.Location = new System.Drawing.Point(265, 240);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(98, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Che za huinya";
             // 
             // Form1
             // 
